e0224266f170f893fe4ab07ea2fd3a47a1574fd1
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-12 18:34:43 +0000

    persistent layer

--- trunk/webapp/lib/phpdao-2.5/generated/class/dto/User.class.php
+++
@@ -1,18 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'user'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-12 19:57	 
-	 */
-	class User{
-		
-		var $userId;
-		var $email;
-		var $password;
-		var $salt;
-		var $firstname;
-		var $lastname;
-		
-	}
-?>
--- trunk/webapp/lib/phpdao-2.5/generated/class/dto/UserHousehold.class.php
+++
@@ -1,16 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'user_household'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-12 19:57	 
-	 */
-	class UserHousehold{
-		
-		var $userHouseholdId;
-		var $userId;
-		var $householdId;
-		var $isOwner;
-		
-	}
-?>

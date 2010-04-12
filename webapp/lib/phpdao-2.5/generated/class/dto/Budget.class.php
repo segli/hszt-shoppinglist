3ec65dc0dd346a7f220c51d93263cbfb005b7058
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-12 18:34:43 +0000

    persistent layer

--- webapp/lib/phpdao-2.5/generated/class/dto/Budget.class.php
+++
@@ -1,18 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'budget'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-12 19:57	 
-	 */
-	class Budget{
-		
-		var $budgetId;
-		var $timeStart;
-		var $timeEnd;
-		var $budgetCurrent;
-		var $budgetQuota;
-		var $householdId;
-		
-	}
-?>
--- webapp/lib/phpdao-2.5/generated/class/dto/List.class.php
+++
@@ -1,16 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'list'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-12 19:57	 
-	 */
-	class List{
-		
-		var $listId;
-		var $name;
-		var $status;
-		var $householdId;
-		
-	}
-?>

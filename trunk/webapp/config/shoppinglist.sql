e5cc401a24f2a81fa30121c9e836a4adf4def359
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-12 17:39:37 +0000

    Database SQL

---
+++ trunk/webapp/config/shoppinglist.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 12, 2010 at 07:38 PM
+-- Server version: 5.1.37
+-- PHP Version: 5.2.11
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `shoppinglist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `bill_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `cost` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `list_id` mediumint(8) unsigned NOT NULL,
+  `user_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`bill_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `bill`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `budget`
+--
+
+CREATE TABLE `budget` (
+  `budget_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `time_start` date NOT NULL,
+  `time_end` date NOT NULL,
+  `budget_current` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `budget_quota` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `household_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`budget_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `budget`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `household`
+--
+
+CREATE TABLE `household` (
+  `household_id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`household_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `household`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `price` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `status` tinyint(3) unsigned NOT NULL,
+  `list_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `item`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `list`
+--
+
+CREATE TABLE `list` (
+  `list_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `status` tinyint(3) unsigned NOT NULL,
+  `household_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`list_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `list`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `salt` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastname` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `user`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_household`
+--
+
+CREATE TABLE `user_household` (
+  `user_household_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(8) unsigned NOT NULL,
+  `household_id` mediumint(8) unsigned NOT NULL,
+  `is_owner` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`user_household_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `user_household`
+--
+

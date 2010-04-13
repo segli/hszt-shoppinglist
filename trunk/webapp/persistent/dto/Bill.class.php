4dcaac17847d5c2de3c24024228b57039bb2d90d
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-13 19:37:50 +0000

    remove old dao files

--- trunk/webapp/persistent/dto/Bill.class.php
+++
@@ -1,17 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'bill'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-12 20:35	 
-	 */
-	class Bill{
-		
-		var $billId;
-		var $date;
-		var $cost;
-		var $listId;
-		var $userId;
-		
-	}
-?>

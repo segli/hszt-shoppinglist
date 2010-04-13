1d316d9353ed14150e775ae2a0a029909f5847f5
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-13 19:37:50 +0000

    remove old dao files

--- webapp/persistent/dto/Household.class.php
+++
@@ -1,14 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'household'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-12 20:35	 
-	 */
-	class Household{
-		
-		var $householdId;
-		var $name;
-		
-	}
-?>

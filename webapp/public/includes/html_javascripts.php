750c2eba8c6fcfa7a46a5f9e0faf6067620bdc9d
thomas.junghans <thomas.junghans@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-18 12:20:20 +0000

    Added register view, changed css structure.

---
+++ webapp/public/includes/html_javascripts.php
@@ -0,0 +1,3 @@
+        <script type="text/javascript" src="javascripts/jquery-1.4.2.min.js"></script>
+        <script type="text/javascript" src="javascripts/shoppinglist.js"></script>
+        <script type="text/javascript" src="javascripts/shoppinglist.mod.login.js"></script>
--- webapp/public/register.php
+++ webapp/public/register.php
@@ -4,10 +4,10 @@ include_once('../config/environment.php');
 <?php
 include_once('includes/html_head.php');?>
     <body>
-        <h1 class="base">Shopping List - Login</h1>
+        <h1 class="base">Shopping List - Register</h1>
 
         <?php
-        include_once('modules/mod.login.php');
+        include_once('modules/mod.register.php');
         ?>
 
     <?php include_once('includes/html_javascripts.php');?>        

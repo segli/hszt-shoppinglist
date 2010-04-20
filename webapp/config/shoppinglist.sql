b9036c7aa02a1cf6c07cbcc7e123cb1fd0e2d2d3
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-20 22:12:40 +0000

    add insert household für thomas und simon

--- webapp/config/shoppinglist.sql
+++ webapp/config/shoppinglist.sql
@@ -3,18 +3,6 @@
 -- Host: localhost    Database: shoppinglist
 -- ------------------------------------------------------
 -- Server version	5.1.37
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `bill`
 --
@@ -89,10 +77,10 @@ CREATE TABLE `household` (
 -- Dumping data for table `household`
 --
 
-LOCK TABLES `household` WRITE;
-/*!40000 ALTER TABLE `household` DISABLE KEYS */;
-/*!40000 ALTER TABLE `household` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `household` VALUES(1, 'JSWG');
+INSERT INTO `household` VALUES(2, 'Simon Private');
+INSERT INTO `household` VALUES(3, 'Thomas Private');
+INSERT INTO `household` VALUES(4, 'Family');
 
 --
 -- Table structure for table `invitation`
@@ -194,11 +182,8 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'simon','ccf7e9e8f7e8a763b26d739dc4ce31a2b61f168b','1234','Simon','Egli'),(2,'thomas','0c5a36f8c1150b5960a56ef534f29320672f5fba','1234','Thomas','Junghans');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `user` VALUES(1, 'simon', 'ccf7e9e8f7e8a763b26d739dc4ce31a2b61f168b', '1234', 'Simon', 'Egli');
+INSERT INTO `user` VALUES(2, 'thomas', '0c5a36f8c1150b5960a56ef534f29320672f5fba', '1234', 'Thomas', 'Junghans');
 
 --
 -- Table structure for table `user_household`
@@ -219,19 +204,9 @@ CREATE TABLE `user_household` (
 --
 -- Dumping data for table `user_household`
 --
+INSERT INTO `user_household` VALUES(1, 1, 1, 1);
+INSERT INTO `user_household` VALUES(2, 1, 2, 1);
+INSERT INTO `user_household` VALUES(3, 2, 3, 1);
+INSERT INTO `user_household` VALUES(4, 2, 4, 1);
 
-LOCK TABLES `user_household` WRITE;
-/*!40000 ALTER TABLE `user_household` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_household` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-04-20 21:16:39

415325e9472c022ddb855df3742333e63af51db7
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-20 20:08:58 +0000

    neues db schema mit inventation table! kann direkt importiert werden. alte db wird gelöscht - neue angelegt! user/pw werden inserted: thomas / thomas, simon / simon

--- trunk/webapp/config/shoppinglist.sql
+++ trunk/webapp/config/shoppinglist.sql
@@ -1,30 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 3.2.0.1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.1.37, for apple-darwin8.11.1 (i386)
 --
--- Host: localhost
--- Generation Time: Apr 20, 2010 at 08:04 PM
--- Server version: 5.1.37
--- PHP Version: 5.2.11
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+-- Host: localhost    Database: shoppinglist
+-- ------------------------------------------------------
+-- Server version	5.1.37
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `shoppinglist`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `bill`
 --
+DROP DATABASE IF EXISTS `shoppinglist`; 
+CREATE DATABASE `shoppinglist`;
+USE `shoppinglist`;
 
+DROP TABLE IF EXISTS `bill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bill` (
   `bill_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
@@ -32,19 +32,25 @@ CREATE TABLE `bill` (
   `shoppinglist_id` mediumint(8) unsigned NOT NULL,
   `user_id` mediumint(8) unsigned NOT NULL,
   PRIMARY KEY (`bill_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `bill`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `bill` WRITE;
+/*!40000 ALTER TABLE `bill` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bill` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `budget`
 --
 
+DROP TABLE IF EXISTS `budget`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `budget` (
   `budget_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `time_start` date NOT NULL,
@@ -53,55 +59,73 @@ CREATE TABLE `budget` (
   `budget_quota` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `household_id` mediumint(8) unsigned NOT NULL,
   PRIMARY KEY (`budget_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `budget`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `budget` WRITE;
+/*!40000 ALTER TABLE `budget` DISABLE KEYS */;
+/*!40000 ALTER TABLE `budget` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `household`
 --
 
+DROP TABLE IF EXISTS `household`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `household` (
   `household_id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`household_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `household`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `household` WRITE;
+/*!40000 ALTER TABLE `household` DISABLE KEYS */;
+/*!40000 ALTER TABLE `household` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `invitation`
 --
 
+DROP TABLE IF EXISTS `invitation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `invitation` (
-  `inventation_id` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `user_id` mediumint(9) NOT NULL,
-  `household_id` mediumint(9) NOT NULL,
-  `pending` tinyint(4) NOT NULL,
+  `inventation_id` mediumint(9) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(9) unsigned NOT NULL,
+  `household_id` mediumint(9) unsigned NOT NULL,
+  `pending` tinyint(4) unsigned NOT NULL,
   PRIMARY KEY (`inventation_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `invitation`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `invitation` WRITE;
+/*!40000 ALTER TABLE `invitation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invitation` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `item`
 --
 
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `item` (
   `item_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
@@ -110,38 +134,50 @@ CREATE TABLE `item` (
   `status` tinyint(3) unsigned NOT NULL,
   `shoppinglist_id` mediumint(8) unsigned NOT NULL,
   PRIMARY KEY (`item_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `item`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `shoppinglist`
 --
 
+DROP TABLE IF EXISTS `shoppinglist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shoppinglist` (
   `shoppinglist_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
   `status` tinyint(3) unsigned NOT NULL,
   `household_id` mediumint(8) unsigned NOT NULL,
   PRIMARY KEY (`shoppinglist_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `shoppinglist`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `shoppinglist` WRITE;
+/*!40000 ALTER TABLE `shoppinglist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shoppinglist` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `user_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
@@ -151,30 +187,51 @@ CREATE TABLE `user` (
   `lastname` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` VALUES(1, 'simon', 'simon', 'simon', 'Simon', 'Egli');
-INSERT INTO `user` VALUES(2, 'thomas', 'thomas', 'thomas', 'Thomas', 'Junghans');
-
--- --------------------------------------------------------
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'simon','ccf7e9e8f7e8a763b26d739dc4ce31a2b61f168b','1234','Simon','Egli'),(2,'thomas','0c5a36f8c1150b5960a56ef534f29320672f5fba','1234','Thomas','Junghans');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user_household`
 --
 
+DROP TABLE IF EXISTS `user_household`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_household` (
   `user_household_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` mediumint(8) unsigned NOT NULL,
   `household_id` mediumint(8) unsigned NOT NULL,
   `is_owner` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`user_household_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user_household`
 --
 
+LOCK TABLES `user_household` WRITE;
+/*!40000 ALTER TABLE `user_household` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_household` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-04-20 21:16:39

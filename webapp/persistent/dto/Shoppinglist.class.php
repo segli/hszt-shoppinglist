a97982569273cca31cdf006ed6ed1d55c8dec1b7
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-20 20:11:26 +0000

    delete old persistent layer

--- webapp/persistent/dto/Shoppinglist.class.php
+++
@@ -1,16 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'shoppinglist'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-13 22:30	 
-	 */
-	class Shoppinglist{
-		
-		var $shoppinglistId;
-		var $name;
-		var $status;
-		var $householdId;
-		
-	}
-?>

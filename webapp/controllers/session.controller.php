ee659f6bd539af0c2a3194e50b61864a81d57b51
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-20 20:13:51 +0000

    session.controller (muss in jedem auth php file included werden!) und logout controller -> wird von logout.php included...

---
+++ webapp/controllers/session.controller.php
@@ -0,0 +1,13 @@
+<?php
+include('lib/authentication.class.php');
+
+session_name("ShopList");
+session_start();
+
+if( ! isset($_SESSION['isAuth']) OR $_SESSION['isAuth'] != 1) {
+    session_destroy();
+    header('Location: index.php');
+}
+
+
+exit;

a156d1958d48beddfb2e5aeb1fda8f45f8b700ec
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-20 20:11:26 +0000

    delete old persistent layer

--- trunk/webapp/persistent/dto/Household.class.php
+++
@@ -1,14 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'household'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-13 22:30	 
-	 */
-	class Household{
-		
-		var $householdId;
-		var $name;
-		
-	}
-?>

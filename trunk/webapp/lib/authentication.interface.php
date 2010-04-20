b7fff8bf204cae80fb4e6ebae2fa1ce151015fe9
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-20 20:06:17 +0000

    - is_authenticated wird nicht benötigt.... user wird anhand isAuth in der session geprüft.

--- trunk/webapp/lib/authentication.interface.php
+++ trunk/webapp/lib/authentication.interface.php
@@ -1,5 +1,5 @@
 <?php
 interface iAuthentication {
     public static function authenticate_user($email, $password);
-    public static function is_user_authenticated($id);
+    //public static function is_user_authenticated($id);
 }

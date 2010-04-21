6848339edb282fcc7362a49048997d1f266a9e0f
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-21 21:21:58 +0000

    insert js for shoppinglists

--- trunk/webapp/public/includes/html_javascripts.php
+++ trunk/webapp/public/includes/html_javascripts.php
@@ -3,3 +3,4 @@
         <script type="text/javascript" src="javascripts/shoppinglist.mod.login.js"></script>
         <script type="text/javascript" src="javascripts/shoppinglist.mod.register.js"></script>
         <script type="text/javascript" src="javascripts/shoppinglist.mod.households.js"></script>
+        <script type="text/javascript" src="javascripts/shoppinglist.mod.shoppinglists.js"></script>

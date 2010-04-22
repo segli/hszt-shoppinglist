62d713629ecd196fe1bdc820721024cdebec448c
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-22 14:50:07 +0000

    old dao files

--- webapp/persistent/dto/Household.class.php
+++
@@ -1,14 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'household'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-20 20:47	 
-	 */
-	class Household{
-		
-		var $householdId;
-		var $name;
-		
-	}
-?>
--- webapp/persistent/dto/Shoppinglist.class.php
+++
@@ -1,16 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'shoppinglist'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-20 20:47	 
-	 */
-	class Shoppinglist{
-		
-		var $shoppinglistId;
-		var $name;
-		var $status;
-		var $householdId;
-		
-	}
-?>

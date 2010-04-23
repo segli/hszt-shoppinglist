2e2ed14c7d1fc54898d0076be9492984c4b3efbe
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-23 19:47:06 +0000

    Extended SQL queries: Item - queryAllByUserIdAndShoppinglistId Item - queryAllByUserId Shoppinglist - queryAllByUserIdAndHouseholdId Shoppinglist - queryAllByUserId

--- trunk/webapp/persistent/mysql/ext/ItemMySqlExtDAO.class.php
+++ trunk/webapp/persistent/mysql/ext/ItemMySqlExtDAO.class.php
@@ -7,6 +7,32 @@
  */
 class ItemMySqlExtDAO extends ItemMySqlDAO{
 
+    public function queryAllByUserIdAndShoppinglistId($uid, $sid){
+
+		$sql = 'SELECT i.* FROM user_household u, household h, shoppinglist s, item i';
+        $sql .= 'WHERE i.shoppinglist_id = s.shoppinglist_id';
+        $sql .= 'AND s.household_id = h.household_id';
+        $sql .= 'AND u.household_id = h.household_id';
+        $sql .= 'AND u.user_id = ?';
+        $sql .= 'AND s.household_id = ?';
+
+		$sqlQuery = new SqlQuery($sql);
+		$sqlQuery->set($uid);
+        $sqlQuery->set($sid);
+		return $this->getList($sqlQuery);
+	}
+
+    public function queryAllByUserId($uid){
+
+		$sql = 'SELECT i.* FROM user_household u, household h, shoppinglist s, item i';
+		$sql .= ' WHERE u.user_id = ?';
+        $sql .= ' AND u.household_id = h.household_id';
+        $sql .= ' AND h.household_id = s.household_id';
+
+		$sqlQuery = new SqlQuery($sql);
+		$sqlQuery->set($uid);
+		return $this->getList($sqlQuery);
+	}
 	
 }
 ?>

d8a01cb3fd41e594a120733e5bcd87ffcb308907
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-23 19:47:06 +0000

    Extended SQL queries: Item - queryAllByUserIdAndShoppinglistId Item - queryAllByUserId Shoppinglist - queryAllByUserIdAndHouseholdId Shoppinglist - queryAllByUserId

--- webapp/persistent/mysql/ext/ShoppinglistMySqlExtDAO.class.php
+++ webapp/persistent/mysql/ext/ShoppinglistMySqlExtDAO.class.php
@@ -7,6 +7,19 @@
  */
 class ShoppinglistMySqlExtDAO extends ShoppinglistMySqlDAO{
 
+     public function queryAllByUserIdAndHouseholdId($uid, $hid){
+
+		$sql = 'SELECT s.* FROM user_household u, household h, shoppinglist s';
+		$sql .= ' WHERE u.user_id = ? AND s.household_id = ?';
+        $sql .= ' AND u.household_id = h.household_id';
+        $sql .= ' AND h.household_id = s.household_id';
+
+		$sqlQuery = new SqlQuery($sql);
+		$sqlQuery->set($uid);
+        $sqlQuery->set($hid);
+		return $this->getList($sqlQuery);
+	}
+
     public function queryAllByUserId($value){
 
 		$sql = 'SELECT s.* FROM user_household u, household h, shoppinglist s';

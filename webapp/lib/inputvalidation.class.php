dfba6fec787ae4794f7ec636285948f3fbd44162
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-24 10:46:28 +0000

    input validation filter implemented filters POST and GET array must be included in every controller with POST and GET data

---
+++ webapp/lib/inputvalidation.class.php
@@ -0,0 +1,41 @@
+<?php
+
+    /*
+    *   Input validation filter for the POST and GET array
+    *   GET - Only a-z and numbers allowed
+    *   POST - Special characters allowed but without '"<> etc.
+    */
+    while (list($key, $val) = each($_GET)) {
+		if(preg_match('/[^a-zA-Z0-9]/', $val )) {
+            unset($_GET[$key]);
+		}
+	}
+
+
+    while (list($key, $val) = each($_POST)) {
+
+        // Whitelist approach
+		if(preg_match('/[^a-zA-Z 0-9@\.\-_]/', $val )) {
+
+            // Special regex for the password field to allow special characters
+            if($key == "user_password") {
+                if(preg_match('/[^a-zA-Z0-9\+!\*@\(\),;\?&=\$_\.]/', $val)) {
+                    echo $key ." - " . $_POST[$key] . "\n";
+                    unset($_POST[$key]);
+                }
+
+            } else {
+                echo $key ." - " . $_POST[$key] . "\n";
+                unset($_POST[$key]);
+            }
+		}
+
+        /*
+        // Blackbox approach
+        if(preg_match('/[\'"<>]/', $val )) {
+            echo $val;
+            unset($_POST[$key]);
+		}
+		*/
+
+	}

4028c3af554b3b4d5687c4827835f60dfba59951
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-24 08:55:26 +0000

    new functions to validate user registration: hash_password generate_salt is_email_address password_confirmation password_complexity

--- webapp/lib/authentication.class.php
+++ webapp/lib/authentication.class.php
@@ -20,7 +20,7 @@ class Authentication implements iAuthentication {
         
 		if(count($user) == 1) {
 
-            $hash = sha1($password . $user[0]->salt);
+            $hash = hash("sha256", $password . $user[0]->salt);
            
 			if($user[0]->password == $hash) {
 				return $user[0];
@@ -31,4 +31,87 @@ class Authentication implements iAuthentication {
             return null;
         }
     }
+
+    /**
+     * @static
+     * @param  {object} $user
+     * @return string
+     */
+    public static function insert_user($user) {
+
+        if(count(DAOFactory::getUserDAO()->queryByEmail($user->email)) == 0) {
+
+            $salt = Authentication::generate_salt();
+            $hash = Authentication::hash_password($user->password, $salt);
+
+            $user->salt = $salt;
+            $user->password = $hash;
+
+            $id = DAOFactory::getUserDAO()->insert($user);
+            return $id;
+            
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @static
+     * @param  {string} $password
+     * @param  {string} $salt
+     * @return string $hash
+     */
+    public static function hash_password($password, $salt) {
+        return hash("sha256", $password . $salt);
+    }
+
+    /**
+     * @static
+     * @return string $salt
+     */
+    public static function generate_salt() {
+        return substr(sha1(uniqid(microtime()) . $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']), 3, 13);
+    }
+
+    /**
+    * @static
+    * @return boolean
+    */
+    public static function is_email_address($email) {
+		if(eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+    /**
+    * @static
+    * @return boolean
+    */
+    public static function password_confirmation($pw1, $pw2) {
+        if ($pw1 == $pw2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+    * @static
+    * @return boolean
+    */
+    public static function password_complexity($password) {
+
+        return true;
+        /*
+        if (strlen($password) < 6) {
+			return false;
+		} else {
+			return true;
+        }*/
+    }
+
+
+    
 }
--- webapp/lib/authentication.interface.php
+++ webapp/lib/authentication.interface.php
@@ -1,5 +1,9 @@
 <?php
 interface iAuthentication {
     public static function authenticate_user($email, $password);
-    //public static function is_user_authenticated($id);
+    public static function hash_password($password, $salt);
+    public static function generate_salt();
+    public static function is_email_address($email);
+    public static function password_confirmation($pw1, $pw2);
+    public static function password_complexity($password);
 }

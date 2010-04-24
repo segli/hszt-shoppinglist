7583dd39b0f6df0744f1e4a77167a081256cc3cd
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-24 08:54:38 +0000

    session handling fixed

--- webapp/controllers/login.controller.php
+++ webapp/controllers/login.controller.php
@@ -1,5 +1,6 @@
 <?php
-include('../lib/authentication.class.php');
+include_once('../lib/authentication.class.php');
+include_once('../lib/inputvalidation.class.php');
 
 // POST / GET variables
 $id = $_POST['user_email'];
@@ -9,40 +10,40 @@ $password = $_POST['user_password'];
 $user = new User();
 $user = Authentication::authenticate_user($id, $password);
 
-if($user != null) {
+    if($user != null) {
 
-    // Set the session cookie name and destroy the old session.
-    session_name("ShopList");
-    session_start();
-    session_destroy();
+        // Set the session cookie name and destroy the old session.
+        session_name("ShopList");
+        session_start();
+        session_destroy();
 
-    // Generate a new session id
-    $session_id = sha1(uniqid(microtime()) . $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']);
-    session_id($session_id);
-    session_start();
+        // Generate a new session id
+        $session_id = sha1(uniqid(microtime()) . $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']);
+        session_id($session_id);
+        session_start();
 
-    // Set session data
-    $_SESSION['isAuth'] = 1;
-    $_SESSION['user'] = $user;
+        // Set session data
+        $_SESSION['isAuth'] = 1;
+        $_SESSION['user'] = $user;
 
-    // Prepare Data
-    $data = array(
-        'user_id' => $user->userId,
-        'email' => $user->email,
-        'firstname' => $user->firstname,
-        'lastname' => $user->lastname,
-        'session_id' => $session_id
-    );
+        // Prepare Data
+        $data = array(
+            'user_id' => $user->userId,
+            'email' => $user->email,
+            'firstname' => $user->firstname,
+            'lastname' => $user->lastname,
+            'session_id' => $session_id
+        );
 
-} else {
+    } else {
 
-    // Prepare Data
-    $data = array(
-        'error' => '1',
-        'message' => 'Incorrect creditentials'
-    );
+        // Prepare Data
+        $data = array(
+            'error' => '1',
+            'message' => 'Incorrect creditentials'
+        );
 
-}
+    }
 
 // Convert to JSON
 $json = json_encode($data);

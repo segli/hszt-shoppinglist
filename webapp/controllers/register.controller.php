b3f0ac357b7db231060c27920c67efce963b5a37
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-24 08:56:46 +0000

    - functions to validate user data moved into the authentication class. - input validation class added --> validate post und get parameter

--- webapp/controllers/register.controller.php
+++ webapp/controllers/register.controller.php
@@ -1,37 +1,54 @@
 <?php
-include_once('lib/include_dao.php');
+include_once('../lib/authentication.class.php');
+include_once('../lib/inputvalidation.class.php');
 
 // POST / GET variables
-$first_name = $_POST['user_first_name'];
-$last_name = $_POST['user_last_name'];
-$email = $_POST['user_email'];
-$password = $_POST['user_password'];
-$confirm_password = $_POST['user_confirm_password'];
-
-// Logic
 $user = new User();
-$user->email = $email;
-$user->password = $password;
-$user->firstname = $first_name;
-$user->lastname = $last_name;
+$user->firstname = $_POST['user_first_name'];
+$user->lastname = $_POST['user_last_name'];
+$user->email = $_POST['user_email'];
+$user->password = $_POST['user_password'];
+$confirm_pw = $_POST['user_confirm_password'];
 
-if(count(DAOFactory::getUserDAO()->queryByEmail($email)) == 0) {
-    $id = DAOFactory::getUserDAO()->insert($user);
+    //Authentication::is_email_address($user->email);
+    if(Authentication::password_confirmation($user->password, $confirm_pw)) {
+       if(Authentication::password_complexity($user->password)) {
 
-    // Prepare Data
-    $data = array(
-        'id' => $id
-    );
+            $id = Authentication::insert_user($user);
 
-} else {
+            if($id != null) {
 
-    // Prepare Data
-    $data = array(
-        'error' => '2',
-        'message' => 'User already exists'
-    );
+                // Prepare Data
+                $data = array(
+                    'id' => $id
+                );
 
-}
+            } else {
+
+                // Prepare Data
+                $data = array(
+                    'error' => '2',
+                    'message' => 'User already exists'
+                );
+            }
+       } else {
+
+            // Prepare Data
+            $data = array(
+                'error' => '2',
+                'message' => 'Password too weak'
+            );
+       }
+    } else {
+
+         // Prepare Data
+        $data = array(
+            'error' => '2',
+            'message' => 'Password does not match'
+        );
+
+    }
+
 
 // Convert to JSON
 $json = json_encode($data);

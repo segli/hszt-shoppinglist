39c6b8e2edbc94d99b37c70ee0e823c1c89189ee
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-26 17:38:20 +0000

    authorization class fixed (schreibfehler)

--- webapp/lib/authorization.class.php
+++ webapp/lib/authorization.class.php
@@ -11,35 +11,28 @@ class Authorization implements iAuthorization {
 
     public static function auth_create_shoppinglist($user_id, $household_id) {
 
-        return true;
-        /*
         $householdlist = DAOFactory::getHouseholdDAO()->queryAllByUserId($user_id);
 
         for($i = 0; $i < count($householdlist); $i++) {
-            if($householdlist[$i]->household_id == $household_id) {
+            if($householdlist[$i]->householdId == $household_id) {
                 return true;
             }
         }
 
         return false;
-        */
     }
 
     public static function auth_create_item($user_id, $shoppinglist_id) {
 
-        return true;
-        /*
-
         $shoppinglists = DAOFactory::getShoppinglistDAO()->queryAllByUserId($user_id);
 
         for($i = 0; $i < count($shoppinglists); $i++) {
-            if($shoppinglists[$i]->shoppinglist_id == $shoppinglist_id) {
+            if($shoppinglists[$i]->shoppinglistId == $shoppinglist_id) {
                 return true;
             }
         }
 
         return false;
-        */
     }
 
 

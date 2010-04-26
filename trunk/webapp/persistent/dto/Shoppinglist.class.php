97e953a190de8daa76fc1171a8695c098794bd94
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-26 21:15:22 +0000

    old dao files

--- trunk/webapp/persistent/dto/Shoppinglist.class.php
+++
@@ -1,19 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'shoppinglist'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-22 16:45	 
-	 */
-	class Shoppinglist{
-		
-		var $shoppinglistId;
-		var $name;
-		var $status;
-		var $dateCreated;
-		var $dateClosed;
-		var $householdId;
-		var $userId;
-		
-	}
-?>

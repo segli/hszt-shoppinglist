7e3c685268b35a139f3d1faf8900813fa29f000d
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-26 18:21:24 +0000

    new query do load invitations

--- trunk/webapp/persistent/mysql/ext/InvitationMySqlExtDAO.class.php
+++ trunk/webapp/persistent/mysql/ext/InvitationMySqlExtDAO.class.php
@@ -7,6 +7,17 @@
  */
 class InvitationMySqlExtDAO extends InvitationMySqlDAO{
 
+    public function queryAllByUserId($value){
+
+		$sql = 'SELECT i.*, h.* FROM invitation i, household h';
+		$sql .= ' WHERE i.user_id = ?';
+        $sql .= ' AND i.household_id = h.household_id';
+        $sql .= ' AND pending = 1';
+
+		$sqlQuery = new SqlQuery($sql);
+		$sqlQuery->set($value);
+		return $this->getList($sqlQuery);
+	}
 	
 }
 ?>

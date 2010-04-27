eaa431a5238166387c970742f77654e0fa68a3f6
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-27 20:38:29 +0000

    invitation fertig

---
+++ trunk/webapp/persistent/dto/InvitationComplete.class.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * Object represents table 'invitation'
+	 *
+     	 * @author: http://phpdao.com
+     	 * @date: 2010-04-26 22:49	 
+	 */
+	class InvitationComplete extends Invitation {
+
+        var $gastgeber; // Vorname + Nachname
+        
+        var $householdName; // Name Household
+	}
+?>
--- trunk/webapp/persistent/mysql/ext/InvitationMySqlExtDAO.class.php
+++ trunk/webapp/persistent/mysql/ext/InvitationMySqlExtDAO.class.php
@@ -8,7 +8,7 @@
 class InvitationMySqlExtDAO extends InvitationMySqlDAO{
 
     // User Id ist in diesem Fall die id des Erstellers!
-    public function queryAllByUserId($uid){
+    public function queryAllByUserId($uid) {
 
 		$sql = 'SELECT i.*, h.* FROM invitation i, household h';
 		$sql .= ' WHERE i.user_id = ?';
@@ -20,9 +20,9 @@ class InvitationMySqlExtDAO extends InvitationMySqlDAO{
 		return $this->getList($sqlQuery);
 	}
 
-    public function queryAllByEmail($email){
+    public function queryAllByEmail($email) {
 
-        $sql = 'SELECT i.*, h.name as household, CONCAT(u.firstname,\' \',u.lastname) as gastgeber';
+        $sql = 'SELECT i.*, h.name as household_name, CONCAT(u.firstname,\' \',u.lastname) as gastgeber';
         $sql .= ' FROM invitation i, household h, user u';
         $sql .= ' WHERE i.user_id = u.user_id ';
         $sql .= ' AND i.email = ?';
@@ -31,7 +31,36 @@ class InvitationMySqlExtDAO extends InvitationMySqlDAO{
 
 		$sqlQuery = new SqlQuery($sql);
 		$sqlQuery->set($email);
-		return $this->getList($sqlQuery);
+		return $this->getListComplete($sqlQuery);
+	}
+
+    /**
+	 * Read row for queryAllByEmail
+	 *
+	 * @return InvitationComplete
+	 */
+	protected function readRowComplete($row) {
+
+		$invitationComplete = new InvitationComplete();
+		$invitationComplete->inventationId  = $row['inventation_id'];
+        $invitationComplete->householdId    = $row['household_id'];
+        $invitationComplete->userId         = $row['user_id'];
+        $invitationComplete->gastgeber      = $row['gastgeber'];
+        $invitationComplete->householdName  = $row['household_name'];
+        $invitationComplete->email          = $row['email'];
+        $invitationComplete->pending        = $row['pending'];
+        $invitationComplete->dateCreated    = $row['date_created'];
+
+		return $invitationComplete;
+	}
+
+	protected function getListComplete($sqlQuery){
+		$tab = QueryExecutor::execute($sqlQuery);
+		$ret = array();
+		for($i=0;$i<count($tab);$i++){
+			$ret[$i] = $this->readRowComplete($tab[$i]);
+		}
+		return $ret;
 	}
 	
 }

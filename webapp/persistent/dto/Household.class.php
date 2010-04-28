4b72f1e5af48743a896ed6a268e34e66ca7a6e14
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-04-28 23:16:14 +0000

    Delete old dao files

--- webapp/persistent/dto/Household.class.php
+++
@@ -1,14 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'household'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-26 22:49	 
-	 */
-	class Household{
-		
-		var $householdId;
-		var $name;
-		
-	}
-?>
--- webapp/persistent/dto/User.class.php
+++
@@ -1,18 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'user'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2010-04-26 22:49	 
-	 */
-	class User{
-		
-		var $userId;
-		var $email;
-		var $password;
-		var $salt;
-		var $firstname;
-		var $lastname;
-		
-	}
-?>

7d0387129a50c42c9239741fa8c57aeb461ded4c
seimnarts <seimnarts@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-05-06 20:29:02 +0000

    Input validation filter included.

--- webapp/controllers/fetchbudget.controller.php
+++ webapp/controllers/fetchbudget.controller.php
@@ -4,6 +4,7 @@ include_once('lib/include_dao.php');
 include_once('lib/message.class.php');
 include_once('lib/authorization.class.php');
 include_once('controllers/session.controller.php');
+include_once('lib/inputvalidation.class.php');
 
 // POST / GET variables
 $user_id = $_SESSION['user']->userId;

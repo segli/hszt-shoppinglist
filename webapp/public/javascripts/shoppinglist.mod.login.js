aafc87e8fc71ec15408ecfb91716de51b303be8e
thomas.junghans <thomas.junghans@b6ba3f93-3c92-b8cf-d462-b139333a32ea>
2010-05-10 22:23:04 +0000

    Fixed paging.

--- webapp/public/javascripts/shoppinglist.mod.login.js
+++ webapp/public/javascripts/shoppinglist.mod.login.js
@@ -1,7 +1,9 @@
+/*global jQuery, log $ */
 var Shoppinglist = Shoppinglist || {};
 
 (Shoppinglist.login = function (options) {
-    var $ctx = null,
+    var $btn_submit = null,
+        $ctx = null,
         init = null,
         config = null,
         defaults = null;
@@ -16,7 +18,7 @@ var Shoppinglist = Shoppinglist || {};
     init = function () {
         
         $ctx = $('.modLogin');
-        
+
         if (Shoppinglist.session) {
             $('.hd', $ctx).hide();
             $('.bd', $ctx).replaceWith('<a href="#" id="go_to_households">Go to households</a>');
@@ -34,21 +36,23 @@ var Shoppinglist = Shoppinglist || {};
         $btn_submit = $('input[type="submit"]', $ctx);
 
         $btn_submit.click(function () {
+        
             $.ajax({
-               'url' : 'controller_proxy.php?controller=login',
-               'data' : $('form',$ctx).serialize(),
-               'type' : 'post',
-               'dataType' : 'json',
-               'success' : function (data) {
+                'url' : 'controller_proxy.php?controller=login',
+                'data' : $('form', $ctx).serialize(),
+                'type' : 'post',
+                'dataType' : 'json',
+                'success' : function (data) {
                   
-                   if (data.session_id) {
-                       Shoppinglist.session = data;
-                       config.onLogin();
-                   } else {
-                       config.onError(data);
-                   }
+                    if (data.session_id) {
+                        Shoppinglist.session = data;
+                        config.onLogin();
+                    } else {
+                        config.onError(data);
+                    }
                 }
             });
+
             return false;
         });
     };
